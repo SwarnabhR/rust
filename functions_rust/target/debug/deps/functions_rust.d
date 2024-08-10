96ee96d9d73604b6e40ccd33340631331c8e57a1
SwarnabhR <swarnabh.roy@gmail.com>
2024-08-10 19:03:44 +0530

    Committed new folder functions_rust

---
+++ functions_rust/target/debug/deps/functions_rust.d
@@ -0,0 +1,5 @@
+C:\Users\swarn\Projects\rust\functions_rust\target\debug\deps\functions_rust.exe: src/main.rs
+
+C:\Users\swarn\Projects\rust\functions_rust\target\debug\deps\functions_rust.d: src/main.rs
+
+src/main.rs:
---
+++ functions_rust/target/debug/functions_rust.d
@@ -0,0 +1,1 @@
+C:\Users\swarn\Projects\rust\functions_rust\target\debug\functions_rust.exe: C:\Users\swarn\Projects\rust\functions_rust\src\main.rs

ba026df4aaa4892f833873589f4248e3048cb019
SwarnabhR <swarnabh.roy@gmail.com>
2024-08-10 19:19:58 +0530

    Commit new changes

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,13 +1,20 @@
-fn main() {
-    let number = 91;
+fn main(){
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
 
-    if number % 4 == 0 {
-        println!("Number is divisible by 4");
-    } else if number % 3 == 0 {
-        println!("Number is divisible by 3");
-    } else if number % 2 == 0 {
-        println!("Number is divisible by 2");
-    } else {
-        println!("Number is not divisible by 2, 3 or 4.");
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2{
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+        count += 1;
     }
+    println!("End count = {count}");
 }

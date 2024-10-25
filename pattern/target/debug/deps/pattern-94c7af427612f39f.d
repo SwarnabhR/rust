275007c4d4187fbf44057ef7b15a35371d323730
Swarnabh Roy <workspace.swarnabh@gmail.com>
2024-10-26 03:15:00 +0530

    Delete pattern/target directory

--- pattern/target/debug/deps/pattern-94c7af427612f39f.d
+++
@@ -1,5 +0,0 @@
-/home/swarnabh/repo/rust/pattern/target/debug/deps/libpattern-94c7af427612f39f.rmeta: src/main.rs
-
-/home/swarnabh/repo/rust/pattern/target/debug/deps/pattern-94c7af427612f39f.d: src/main.rs
-
-src/main.rs:
--- pattern/target/debug/deps/pattern-e04227a515cb4135.d
+++
@@ -1,5 +0,0 @@
-/home/swarnabh/repo/rust/pattern/target/debug/deps/libpattern-e04227a515cb4135.rmeta: src/main.rs
-
-/home/swarnabh/repo/rust/pattern/target/debug/deps/pattern-e04227a515cb4135.d: src/main.rs
-
-src/main.rs:
